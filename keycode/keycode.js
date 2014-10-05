edca2cce8bd63f1749ea5cead8e592fe0a7780f2
Rubén Martínez <rub3nmv@gmail.com>
2014-10-05 22:34:28 +0200

    Add keycode tool

---
+++ keycode/keycode.js
@@ -0,0 +1,14 @@
+/*global window, document, console*/
+'strict';
+
+var code = document.getElementById('keycode'),
+	desc = document.getElementById('code-description');
+
+function setCode(event) {
+	// return false doesn't work on this type of calls
+	// so we use preventDefault() to cancel browser actions (like f1 opening help...)
+	event.preventDefault();
+	code.textContent = event.keyCode;
+}
+
+window.addEventListener('keydown', setCode);
