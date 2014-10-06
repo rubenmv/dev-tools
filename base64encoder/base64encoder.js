83c990482517d79e0908ff67e3834a331dc5b3a0
Rubén Martínez <rub3nmv@gmail.com>
2014-10-06 02:44:00 +0200

    Add base64 encoder

---
+++ base64encoder/base64encoder.js
@@ -0,0 +1,48 @@
+/*global window, document, console, FileReader*/
+'strict';
+
+var FILE_TYPES = ['image/jpeg', 'image/jpg', 'image/png', 'image/x-icon'],
+	FILE_SIZE_LIMIT = 202400; //Bytes, 100KB (1KB = 1024B)
+
+function setFileError(errorString) {
+	document.getElementById('file-error').textContent = errorString;
+}
+
+function reset() {
+	document.getElementById('file-error').textContent = '';
+	document.getElementById('results-string').value = '';
+	document.getElementById('results-html').value = '';
+}
+
+function handleFileSelect(evt) {
+	var filePicker = document.getElementById('file-picker');
+	var files = filePicker.files,
+		file = files[0];
+	if (files && file) {
+		if (file.size > FILE_SIZE_LIMIT) {
+			setFileError('File size limit exceeded');
+			evt.srcElement.value = '';
+			return;
+		}
+		else if (FILE_TYPES.indexOf(file.type) === -1 ) {
+			setFileError('Invalid file type');
+			evt.srcElement.value = '';
+			return;
+		}
+		var reader = new FileReader();
+		reader.onload = function(readerEvt) {
+			var binaryString = readerEvt.target.result,
+				base64String = 'data:image/png;base64,' + window.btoa(binaryString);
+			document.getElementById("results-string").value = base64String;
+			document.getElementById("results-html").value = '<img src="'+base64String+'" />';
+		};
+		reader.readAsBinaryString(file);
+	}
+}
+
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+    document.getElementById('button-convert').addEventListener('click', handleFileSelect, false);
+	document.getElementById('file-picker').addEventListener('click', reset, false);
+} else {
+    setFileError('The File APIs are not fully supported in this browser.');
+}
