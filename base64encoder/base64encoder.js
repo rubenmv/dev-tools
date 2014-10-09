92969b4beddd1c7e79571e3aee3fb5b29029a86e
Rubén Martínez <rub3nmv@gmail.com>
2014-10-09 02:46:00 +0200

    Fix scope
    - Script under function scope
    - Add html5shiv for IE compatibility

--- base64encoder/base64encoder.js
+++ base64encoder/base64encoder.js
@@ -1,51 +1,52 @@
 /*global window, document, console, FileReader*/
-'strict';
+(function() {
+	'use strict';
 
-var FILE_TYPES = ['image/jpeg', 'image/jpg', 'image/png', 'image/x-icon'],
-	FILE_SIZE_LIMIT = 202400; //Bytes, 100KB (1KB = 1024B)
+	var FILE_TYPES = ['image/jpeg', 'image/jpg', 'image/png', 'image/x-icon'],
+		FILE_SIZE_LIMIT = 202400; //Bytes, 100KB (1KB = 1024B)
 
-function setFileError(errorString) {
-	document.getElementById('file-error').textContent = errorString;
-}
-
-function reset() {
-	document.getElementById('file-error').textContent = '';
-	document.getElementById('results-string').value = '';
-	document.getElementById('results-html').value = '';
-}
-
-function handleFileSelect(evt) {
-	var filePicker = document.getElementById('file-picker__input');
-	if(filePicker === null) {
-		return false;
+	function setFileError(errorString) {
+		document.getElementById('file-error').textContent = errorString;
 	}
-	var files = filePicker.files,
-		file = files[0];
-	if (files && file) {
-		if (file.size > FILE_SIZE_LIMIT) {
-			setFileError('File size limit exceeded');
-			evt.srcElement.value = '';
+
+	function reset() {
+		document.getElementById('file-error').textContent = '';
+		document.getElementById('results-string').value = '';
+		document.getElementById('results-html').value = '';
+	}
+
+	function handleFileSelect(evt) {
+		var filePicker = document.getElementById('file-picker__input');
+		if (filePicker === null) {
 			return false;
 		}
-		else if (FILE_TYPES.indexOf(file.type) === -1 ) {
-			setFileError('Invalid file type');
-			evt.srcElement.value = '';
-			return false;
+		var files = filePicker.files,
+			file = files[0];
+		if (files && file) {
+			if (file.size > FILE_SIZE_LIMIT) {
+				setFileError('File size limit exceeded');
+				evt.srcElement.value = '';
+				return false;
+			} else if (FILE_TYPES.indexOf(file.type) === -1) {
+				setFileError('Invalid file type');
+				evt.srcElement.value = '';
+				return false;
+			}
+			var reader = new FileReader();
+			reader.onload = function(readerEvt) {
+				var binaryString = readerEvt.target.result,
+					base64String = 'data:image/png;base64,' + window.btoa(binaryString);
+				document.getElementById("results-string").value = base64String;
+				document.getElementById("results-html").value = '<img src="' + base64String + '" />';
+			};
+			reader.readAsBinaryString(file);
 		}
-		var reader = new FileReader();
-		reader.onload = function(readerEvt) {
-			var binaryString = readerEvt.target.result,
-				base64String = 'data:image/png;base64,' + window.btoa(binaryString);
-			document.getElementById("results-string").value = base64String;
-			document.getElementById("results-html").value = '<img src="'+base64String+'" />';
-		};
-		reader.readAsBinaryString(file);
 	}
-}
 
-if (window.File && window.FileReader && window.FileList && window.Blob) {
-    document.getElementById('button-convert').addEventListener('click', handleFileSelect, false);
-	document.getElementById('file-picker__input').addEventListener('click', reset, false);
-} else {
-    setFileError('The File APIs are not fully supported in this browser.');
-}
+	if (window.File && window.FileReader && window.FileList && window.Blob) {
+		document.getElementById('button-convert').addEventListener('click', handleFileSelect, false);
+		document.getElementById('file-picker__input').addEventListener('click', reset, false);
+	} else {
+		setFileError('The File APIs are not fully supported in this browser.');
+	}
+}());
--- keycode/keycode.js
+++ keycode/keycode.js
@@ -1,14 +1,16 @@
 /*global window, document, console*/
-'strict';
+(function() {
+	'use strict';
 
-var code = document.getElementById('keycode'),
-	desc = document.getElementById('code-description');
+	var code = document.getElementById('keycode'),
+		desc = document.getElementById('code-description');
 
-function setCode(event) {
-	// return false doesn't work on this type of calls
-	// so we use preventDefault() to cancel browser actions (like f1 opening help...)
-	event.preventDefault();
-	code.textContent = event.keyCode;
-}
+	function setCode(event) {
+		// return false doesn't work on this type of calls
+		// so we use preventDefault() to cancel browser actions (like f1 opening help...)
+		event.preventDefault();
+		code.textContent = event.keyCode;
+	}
 
-window.addEventListener('keydown', setCode);
+	window.addEventListener('keydown', setCode);
+}());
